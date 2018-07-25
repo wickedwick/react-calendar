d0aaee5952c4d187efa6182ae0eeb2431164567b
Travis Wickham <wickham@civicplus.com>
2018-07-25 12:19:09 -0500

    Changed getDay() to getDate() for date comparison

--- dist/MonthView/Day.js
+++ dist/MonthView/Day.js
@@ -31,7 +31,7 @@ function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in ob
 var className = 'react-calendar__month-view__days__day';
 var appendTodayClassNameIfNeeded = function appendTodayClassNameIfNeeded(date) {
   var today = new Date();
-  if (date.getYear() === today.getYear() && date.getMonth() === today.getMonth() && date.getDay() === today.getDate()) {
+  if (date.getYear() === today.getYear() && date.getMonth() === today.getMonth() && date.getDate() === today.getDate()) {
     return " today";
   }
   return "";
